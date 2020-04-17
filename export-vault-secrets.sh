d93e10ae6381fe4e823c908ea0c90b6ad9b52d8d
Tsigab Angosom Gebremedhin <tsigab.angosom@gmail.com>
2020-04-17 14:59:40 +0200

    MMA-4322 endret servicebruker til srvskanmotreferanse pga string size over 20.

--- export-vault-secrets.sh
+++ export-vault-secrets.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env sh
 
-if test -f /var/run/secrets/nais.io/srvdokdistavstemming/username;
+if test -f /var/run/secrets/nais.io/srvskanmotreferanse/username;
 then
     echo "Setting SERVICEUSER_USERNAME"
-    export SERVICEUSER_USERNAME=$(cat /var/run/secrets/nais.io/srvdokdistavstemming/username)
+    export SERVICEUSER_USERNAME=$(cat /var/run/secrets/nais.io/srvskanmotreferanse/username)
 fi
-if test -f /var/run/secrets/nais.io/srvdokdistavstemming/password;
+if test -f /var/run/secrets/nais.io/srvskanmotreferanse/password;
 then
     echo "Setting SERVICEUSER_PASSWORD"
-    export SERVICEUSER_PASSWORD=$(cat /var/run/secrets/nais.io/srvdokdistavstemming/password)
+    export SERVICEUSER_PASSWORD=$(cat /var/run/secrets/nais.io/srvskanmotreferanse/password)
 fi
 
 
