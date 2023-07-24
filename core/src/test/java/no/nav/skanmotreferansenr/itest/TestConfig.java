35957230b6335f667e03f82bb4e1f63feb186de0
Joakim Bjørnstad <joakim.bjornstad@nav.no>
2023-07-24 14:15:14 +0200

    MMA-6975 Oppdatert avhengigheter, optimalisert
    * Oppdatert camel, sshd-mina, etc
    * JAXBContext implementasjoner er trådsikre, instansieres kun en gang
    * Fikset sshd oppsett

--- core/src/test/java/no/nav/skanmotreferansenr/itest/TestConfig.java
+++ core/src/test/java/no/nav/skanmotreferansenr/itest/TestConfig.java
@@ -7,12 +7,12 @@ import no.nav.skanmotreferansenr.metrics.DokCounter;
 import org.apache.camel.CamelContext;
 import org.apache.camel.spring.boot.CamelContextConfiguration;
 import org.apache.sshd.common.file.virtualfs.VirtualFileSystemFactory;
+import org.apache.sshd.common.keyprovider.ClassLoadableResourceKeyPairProvider;
+import org.apache.sshd.scp.server.ScpCommandFactory;
 import org.apache.sshd.server.SshServer;
 import org.apache.sshd.server.auth.UserAuthNoneFactory;
-import org.apache.sshd.server.config.keys.AuthorizedKeysAuthenticator;
-import org.apache.sshd.server.keyprovider.SimpleGeneratorHostKeyProvider;
-import org.apache.sshd.server.scp.ScpCommandFactory;
-import org.apache.sshd.server.subsystem.sftp.SftpSubsystemFactory;
+import org.apache.sshd.server.auth.pubkey.AcceptAllPublickeyAuthenticator;
+import org.apache.sshd.sftp.server.SftpSubsystemFactory;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
@@ -22,10 +22,9 @@ import org.springframework.context.annotation.Import;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.concurrent.ThreadLocalRandom;
 import java.util.concurrent.atomic.AtomicInteger;
 
-import static java.nio.file.Path.of;
+import static java.lang.Integer.parseInt;
 import static java.util.Collections.singletonList;
 
 @Slf4j
@@ -35,57 +34,56 @@ import static java.util.Collections.singletonList;
 @Import({CoreConfig.class, TestConfig.CamelTestStartupConfig.class, TestConfig.SshdSftpServerConfig.class, DokCounter.class})
 public class TestConfig {
 
-    @Configuration
-    static class CamelTestStartupConfig {
+	@Configuration
+	static class CamelTestStartupConfig {
 
-        private final AtomicInteger sshServerStartupCounter = new AtomicInteger(0);
-        @Bean
-        CamelContextConfiguration contextConfiguration(SshServer sshServer) {
-            return new CamelContextConfiguration() {
+		private final AtomicInteger sshServerStartupCounter = new AtomicInteger(0);
 
-                @Override
-                public void beforeApplicationStart(CamelContext camelContext) {
-                    while(!sshServer.isStarted() && sshServerStartupCounter.get() <= 5) {
-                        try {
-                            // Busy wait
-                            Thread.sleep(1000);
-                            log.info("Forsøkt å starte sshserver. retry=" + sshServerStartupCounter.getAndIncrement());
-                        } catch (InterruptedException e) {
-                            // noop
-                        }
-                    }
-                }
+		@Bean
+		CamelContextConfiguration contextConfiguration(SshServer sshServer) {
+			return new CamelContextConfiguration() {
 
-                @Override
-                public void afterApplicationStart(CamelContext camelContext) {
+				@Override
+				public void beforeApplicationStart(CamelContext camelContext) {
+					while (!sshServer.isStarted() && sshServerStartupCounter.get() <= 5) {
+						try {
+							// Busy wait
+							Thread.sleep(1000);
+							log.info("Forsøkt å starte sshserver. retry=" + sshServerStartupCounter.getAndIncrement());
+						} catch (InterruptedException e) {
+							// noop
+						}
+					}
+				}
 
-                }
-            };
-        }
-    }
+				@Override
+				public void afterApplicationStart(CamelContext camelContext) {
 
-    @Configuration
-    static class SshdSftpServerConfig {
-        @Bean
-        public Path sshdPath() throws IOException {
-            return Files.createTempDirectory("sshd");
-        }
+				}
+			};
+		}
+	}
 
-        @Bean(initMethod = "start", destroyMethod = "stop")
-        public SshServer sshServer(Path sshdPath) {
+	@Configuration
+	static class SshdSftpServerConfig {
+		@Bean
+		public Path sshdPath() throws IOException {
+			return Files.createTempDirectory("sshd");
+		}
 
-            String sftpPort = String.valueOf(ThreadLocalRandom.current().nextInt(2000, 2999));
-            System.setProperty("skanmotreferansenr.sftp.port", sftpPort);
-
-            SshServer sshd = SshServer.setUpDefaultServer();
-            sshd.setPort(Integer.parseInt(sftpPort));
-            sshd.setKeyPairProvider(new SimpleGeneratorHostKeyProvider(of("src/test/resources/sftp/itest.ser")));
-            sshd.setCommandFactory(new ScpCommandFactory());
-            sshd.setSubsystemFactories(singletonList(new SftpSubsystemFactory()));
-            sshd.setPublickeyAuthenticator(new AuthorizedKeysAuthenticator(of("src/test/resources/sftp/itest_valid.pub")));
-            sshd.setUserAuthFactories(singletonList(new UserAuthNoneFactory()));
-            sshd.setFileSystemFactory(new VirtualFileSystemFactory(sshdPath));
-            return sshd;
-        }
-    }
+		@Bean(initMethod = "start", destroyMethod = "stop")
+		public SshServer sshServer(Path sshdPath,
+								   SkanmotreferansenrProperties skanmotreferansenrProperties) {
+			SshServer sshd = SshServer.setUpDefaultServer();
+			sshd.setPort(parseInt(skanmotreferansenrProperties.getSftp().getPort()));
+			sshd.setKeyPairProvider(new ClassLoadableResourceKeyPairProvider("sftp/server_id_rsa"));
+			sshd.setCommandFactory(new ScpCommandFactory());
+			sshd.setSubsystemFactories(singletonList(new SftpSubsystemFactory()));
+			// aksepterer alle public keys som presenteres, behøver ikke authorized_keys
+			sshd.setPublickeyAuthenticator(AcceptAllPublickeyAuthenticator.INSTANCE);
+			sshd.setUserAuthFactories(singletonList(new UserAuthNoneFactory()));
+			sshd.setFileSystemFactory(new VirtualFileSystemFactory(sshdPath));
+			return sshd;
+		}
+	}
 }
